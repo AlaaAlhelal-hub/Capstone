1f7655299361710af3dbd2a4ac0704ce196d3619
AlaaAlhelal <36053108+AlaaAlhelal@users.noreply.github.com>
2020-12-23 21:29:12 +0300

    version 1

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
-DATABASE_URL=postgres://omhesvoeovnphm:43e439582d233bd204be64e005b04254276ff746707e7f075f198b274316f080@ec2-23-20-168-40.compute-1.amazonaws.com:5432/dc4alsk53pf8nn
-EXCITED=true
+
+export ENV='production'
+
+export FLASK_APP='app'
+
+export FLASK_DEBUG=True
+
+export DATABASE_URL='postgres://omhesvoeovnphm:43e439582d233bd204be64e005b04254276ff746707e7f075f198b274316f080@ec2-23-20-168-40.compute-1.amazonaws.com:5432/dc4alsk53pf8nn'
+
+flask db upgrade
